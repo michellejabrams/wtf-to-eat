f3d91701f7085eaf345f9c2063acc0d19538e8a9
Michelle <michellejabrams@gmail.com>
2015-07-13 10:22:39 -0400

    Adds additional resturant info

--- src/newApp.js
+++ src/newApp.js
@@ -11,32 +11,51 @@ var LunchPicker = React.createClass({
 		    "name": "Choza",
 		    "address": "66 Madison Avenue, New York, NY 10016",
 		    "phone": "212-213-0708",
-		    "type": "Mexican"
+		    "type": "Mexican",
+		    "link": "http://www.seamless.com/food-delivery/choza-new-york-city.18160.r?a=966"
 		},
 		{
 		    "name": "Dos Toros",
 		    "address": "295 Park Ave South, New York, NY 10010",
 		    "phone": "347-946-6225",
-		    "type": "Mexican"
+		    "type": "Mexican",
+		    "link": "http://www.dostoros.com/"
 		},
 		{
 		    "name": "DiDi Dumpling",
 		    "address": "38 Lexington Avenue, New York, NY 10010",
 		    "phone": "212-466-6618",
-		    "type": "Chinese"
+		    "type": "Chinese",
+		    "link": "http://www.dididumplings.com/"
 		},
 		{
 		    "name": "Mamagyro",
 		    "address": "820 Broadway, New York, NY 10003",
 		    "phone": "212-477-1717",
-		    "type": "Greek"
+		    "type": "Greek",
+		    "link": "http://www.seamless.com/food-delivery/mamagyro-new-york-city.17908.r"
 
 		},
 		{
 		    "name": "Tiffin Wallah",
 		    "address": "127 E 28th Street, New York, NY 10016",
 		    "phone": "212-685-7301",
-		    "type": "Indian"
+		    "type": "Indian",
+		    "link": "http://www.seamless.com/food-delivery/tiffin-wallah-new-york-city.8113.r?a=966"
+		},
+		{
+		    "name": "Sophie's Cuban Cuisine",
+		    "address": "28 E 23rd Street, New York, NY 10010",
+		    "phone": "212-260-8884",
+		    "type": "Cuban",
+		    "link": "http://www.seamless.com/food-delivery/sophie-cuban-cuisine-23rd-st-new-york-city.11353.r?a=966"
+		},
+		{
+		    "name": "Chop't",
+		    "address": "18 E 23rd St, New York, NY 10010",
+		    "phone": "646-755-7837",
+		    "type": "Salad Bar",
+		    "link": "http://www.seamless.com/food-delivery/chop-t-creative-salad-co-flatiron-new-york-city.22115.r"
 		}
 		];
 
@@ -56,34 +75,42 @@ var LunchPicker = React.createClass({
 
 	render: function() {
 		var randomRestaurant = this.state.randomRestaurant;
-		var randomRestaurantInfo;
+		var restaurantInfo;
 		if(randomRestaurant) {
-			randomRestaurantInfo = (
-				<div className="restaurant-name-wrapper">
-	                <h2>{randomRestaurant.name}</h2>
-	                <p>Address: <span>{randomRestaurant.address}</span></p>
-	                <p>Phone: <span>{randomRestaurant.phone}</span></p>
-	                <p>Type: <span>{randomRestaurant.type}</span></p>
+			restaurantInfo = (
+				<div className="restaurant-info">
+	                <h2 className="restaurant-name">{randomRestaurant.name}</h2>
+	                <p><strong className="info-label">Address:</strong> <span>{randomRestaurant.address}</span></p>
+	                <p><strong className="info-label">Phone:</strong> <span>{randomRestaurant.phone}</span></p>
+	                <p><strong className="info-label">Type:</strong> <span>{randomRestaurant.type}</span></p>
+	                <p><a className="order-link" href={randomRestaurant.link} target="_blank">ORDER THAT SHIT!</a></p>
 				</div>
 			);
 		}
 
 		return (
-			<div className="container">
+			<div className="container lunchpicker-container">
 				<div className="row">
 					<div className="col-xs-12">
-						<div className="button-wrapper">
-							<h1>What the f*%$ should ATTCK eat for lunch!?</h1>
-							<button onClick={this.generateRandomRestaurant }>Find me something</button>
+						<div className="wtf-wrapper">
+							<h1>
+								What the f&amp;*k should <br />
+								<img src="img/attck_logo.png" className="attck-logo" /> <br />
+								eat for lunch!?
+							</h1>
+							<button className="btn" onClick={this.generateRandomRestaurant }>
+								Find me something
+							</button>
 						</div>
-						{ randomRestaurantInfo }
+						<div className="restaurant-info-wrapper">
+							{ restaurantInfo }
+						</div>
 					</div>
 				</div>
 			</div>
 		);
 	}
 });
-
 
 React.render (
 	<LunchPicker />,
